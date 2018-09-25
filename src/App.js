40574cbb5bd1d015bb198fd8d19291f2a56c7100
Tonsai jompun <tonsai.j@nexts-corp.com>
2018-09-25 11:49:15 +0700

    update pitt

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React from "react";
+import React from 'react'
 // import logo from '../assets/images/machine-learning-cheet-sheet.png'
-import { hot } from "react-hot-loader";
+import { hot } from 'react-hot-loader'
 import Header from './components/Header'
 import Routes from './components/Routes'
 
-
 const App = () => (
   <div className="my-app">
-    <Header>
-      test
-    </Header>
+    <Header>test</Header>
     <div className="App container">
-      <Routes/>
+      <Routes />
     </div>
   </div>
-);
+)
 // export default App
-export default hot(module)(App);
+export default hot(module)(App)
