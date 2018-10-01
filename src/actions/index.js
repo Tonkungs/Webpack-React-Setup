31cbd5be49a23deab3de9e8b56ca951fded43739
Tonsai jompun <tonsai.j@nexts-corp.com>
2018-10-01 21:41:12 +0700

    เอาออก

--- src/actions/index.js
+++ src/actions/index.js
@@ -27,7 +27,7 @@ export const subCount = payload => {
 };
 
 const fetchPosts = subreddit => dispatch => {
-  return fetch("https://murmuring-citadel-13024.herokuapp.com/api/v1/iguser", {
+  return fetch("", {
     mode: "cors"
   })
     .then(response => response.json())
